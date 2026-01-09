0e21feeb4c721840739155a2f3fbadd67f778977
D Harithamma <harithamma.d@ibm.com>
2026-01-09 11:39:27 -0500

    initial commit with p521_kyber1024 enabled

---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux') {
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/oqs-providerport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/oqs-providerport.git'),
+      string(name: 'PORT_DESCRIPTION', value: 'This repository contains code to enable quantum-safe cryptography (QSC) in a standard OpenSSL (3.x) distribution by way of implementing a single shared library, the OQS'),
+      string(name: 'BUILD_LINE', value: 'DEV')
+    ]
+  }
+}
